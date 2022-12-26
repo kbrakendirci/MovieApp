4a8a65cb913a07e5d81681a23a61130c93db5baa
kkendirci <kubra.kendirci@btc-ag.com.tr>
2022-12-26 23:33:52 +0300

    Created Bottom Navigation

---
+++ MovieApp/app/src/main/java/com/example/movieapp/ui/MovieFavoritesFragment.kt
@@ -0,0 +1,21 @@
+package com.example.movieapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieapp.R
+
+// TODO: Rename parameter arguments, choose names that match
+
+class MovieFavoritesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_favorites, container, false)
+    }
+
+}
