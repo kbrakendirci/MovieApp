a202ff32e605ae7c3834a2d4ef40d8fc4031aaf7
kkendirci <kubra.kendirci@btc-ag.com.tr>
2022-12-26 23:35:38 +0300

    Created MovieFavoriteFragment and MovieListFragment

--- MovieApp/app/src/main/java/com/example/movieapp/ui/favoritesmovie/MovieFavoritesFragment.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/ui/favoritesmovie/MovieFavoritesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.movieapp.ui
+package com.example.movieapp.ui.favoritesmovie
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.movieapp.R
 
+// TODO: Rename parameter arguments, choose names that match
 
-class MovieListFragment : Fragment() {
+class MovieFavoritesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+        return inflater.inflate(R.layout.fragment_movie_favorites, container, false)
     }
 
 }
